21adad7bc3187d8ae9faffe178a5c067082adf5c
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2020-11-10 07:25:47 -0700

    update SviQueryConstructor

--- src/components/queryConstructors/SviQueryConstructor.vue
+++ src/components/queryConstructors/SviQueryConstructor.vue
@@ -15,7 +15,8 @@
           <label for="belowPoverty">Below Poverty</label>
         </div>
         <div class="col-4">
-          <input type="number" class="form-control form-inline" id="belowPoverty" value="1">
+          <input type="number" class="form-control form-inline" id="belowPoverty" value="1" v-model="belowPoverty"
+                 v-on:change="valuesUpdated">
         </div>
       </div>
     </div>
@@ -25,7 +26,8 @@
         <label for="unemployed">Unemployed</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="unemployed" value="1">
+        <input type="number" class="form-control form-inline" id="unemployed" value="1" v-model="unemployed"
+               v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- Income -->
@@ -34,7 +36,8 @@
         <label for="income">Income</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="income" value="1">
+        <input type="number" class="form-control form-inline" id="income" value="1" v-model="income"
+               v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- No High School Diploma -->
@@ -43,7 +46,8 @@
         <label for="noHighSchoolDiploma">No High School Diploma</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="noHighSchoolDiploma" value="1">
+        <input type="number" class="form-control form-inline" id="noHighSchoolDiploma" value="1"
+               v-model="noHighSchoolDiploma" v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- Aged 65 or older -->
@@ -52,7 +56,8 @@
         <label for="aged65OrOlder">Aged 65 or Older</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="aged65OrOlder" value="1">
+        <input type="number" class="form-control form-inline" id="aged65OrOlder" value="1" v-model="aged65OrOlder"
+               v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- Aged 17 or Younger -->
@@ -61,16 +66,18 @@
         <label for="aged17OrYounger">Aged 17 or Younger</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="aged17OrYounger" value="1">
+        <input type="number" class="form-control form-inline" id="aged17OrYounger" value="1" v-model="aged17OrYounger"
+               v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- Older than Age 5 with a Disability -->
     <div class="row form-control-static">
       <div class="col-8">
-        <label for="age5OrOlderWithDisability">Older than Age 5 with a Disability</label>
+        <label for="age5orOlderWithDisability">Older than Age 5 with a Disability</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="age5OrOlderWithDisability" value="1">
+        <input type="number" class="form-control form-inline" id="age5orOlderWithDisability" value="1"
+               v-model="age5orOlderWithDisability" v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- Single-Parent Households -->
@@ -79,7 +86,8 @@
         <label for="singleParentHouseholds">Single Parent Households</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="singleParentHouseholds" value="1">
+        <input type="number" class="form-control form-inline" id="singleParentHouseholds" value="1"
+               v-model="singleParentHouseholds" v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- Minority -->
@@ -88,7 +96,8 @@
         <label for="minority">Minority</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="minority" value="1">
+        <input type="number" class="form-control form-inline" id="minority" value="1" v-model="minority"
+               v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- Speaks English "Less than Well" -->
@@ -97,7 +106,8 @@
         <label for="speaksEnglishLessThanWell">Speaks English "Less than Well"</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="speaksEnglishLessThanWell" value="1">
+        <input type="number" class="form-control form-inline" id="speaksEnglishLessThanWell" value="1"
+               v-model="speaksEnglishLessThanWell" v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- Multi-Unit Structures -->
@@ -106,7 +116,8 @@
         <label for="multiUnitStructures">Multi-Unit Structures</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="multiUnitStructures" value="1">
+        <input type="number" class="form-control form-inline" id="multiUnitStructures" value="1"
+               v-model="multiUnitStructures" v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- Mobile Homes -->
@@ -115,7 +126,8 @@
         <label for="mobileHomes">Mobile Homes</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="mobileHomes" value="1">
+        <input type="number" class="form-control form-inline" id="mobileHomes" value="1" v-model="mobileHomes"
+               v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- Crowding -->
@@ -124,7 +136,8 @@
         <label for="crowding">Crowding</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="crowding" value="1">
+        <input type="number" class="form-control form-inline" id="crowding" value="1" v-model="crowding"
+               v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- No Vehicle -->
@@ -133,7 +146,8 @@
         <label for="noVehicle">No Vehicle</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="noVehicle" value="1">
+        <input type="number" class="form-control form-inline" id="noVehicle" value="1" v-model="noVehicle"
+               v-on:change="valuesUpdated">
       </div>
     </div>
     <!-- Group Quarters -->
@@ -142,10 +156,10 @@
         <label for="groupQuarters">Group Quarters</label>
       </div>
       <div class="col-4">
-        <input type="number" class="form-control form-inline" id="groupQuarters" value="1">
+        <input type="number" class="form-control form-inline" id="groupQuarters" value="1" v-model="groupQuarters"
+               v-on:change="valuesUpdated">
       </div>
     </div>
-
   </div>
 </template>
 
@@ -156,7 +170,28 @@
 export default {
   name: 'SviQueryConstructor',
   data() {
-    return {}
+    return {
+      belowPoverty: 1,
+      unemployed: 1,
+      income: 1,
+      noHighSchoolDiploma: 1,
+      aged65OrOlder: 1,
+      aged17OrYounger: 1,
+      age5orOlderWithDisability: 1,
+      singleParentHouseholds: 1,
+      minority: 1,
+      speaksEnglishLessThanWell: 1,
+      multiUnitStructures: 1,
+      mobileHomes: 1,
+      crowding: 1,
+      noVehicle: 1,
+      groupQuarters: 1
+    }
+  },
+  methods: {
+    valuesUpdated() {
+      console.log('updated');
+    }
   }
 }
 </script>
